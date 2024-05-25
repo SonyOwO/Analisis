163e7ee9f3556026e95b9610cdff336c60148d72
SonyOwO <164659542+SonyOwO@users.noreply.github.com>
2024-05-25 17:30:30 -0600

    Subida proyecto
    Co-Authored-By: SackBoy08 <164660096+sackboy08@users.noreply.github.com>
    Co-Authored-By: Anthony Avalos <98932340+afernan2001@users.noreply.github.com>

---
+++ carrental/admin/includes/header.php
@@ -0,0 +1,14 @@
+<div class="brand clearfix">
+	<a href="dashboard.php" style="font-size: 25px;">Portal de alquiler de coches | Panel de administrador</a>  
+		<span class="menu-btn"><i class="fa fa-bars"></i></span>
+		<ul class="ts-profile-nav">
+			
+			<li class="ts-account">
+				<a href="#"><img src="img/ts-avatar.jpg" class="ts-avatar hidden-side" alt=""> Cuenta <i class="fa fa-angle-down hidden-side"></i></a>
+				<ul>
+					<li><a href="change-password.php">Cambiar contraseña</a></li>
+					<li><a href="logout.php">Salir</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
---
+++ carrental/admin/includes/leftbar.php
@@ -0,0 +1,40 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Menu</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Panel</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Marcas</a>
+<ul>
+<li><a href="create-brand.php">Crear marca</a></li>
+<li><a href="manage-brands.php">Administrar marcas</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-car"></i> Vehículos	</a>
+					<ul>
+						<li><a href="post-avehical.php">Publicar un vehículo</a></li>
+						<li><a href="manage-vehicles.php">Administrar vehículos</a></li>
+					</ul>
+				</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Reservaciones</a>
+					<ul>
+						<li><a href="new-bookings.php">Nuevo</a></li>
+						<li><a href="confirmed-bookings.php">Confirmado</a></li>
+						<li><a href="canceled-bookings.php">Cancelado</a></li>
+					</ul>
+				</li>
+
+		
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Gestionar testimonios</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop"></i> Administrar consulta de contacto</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Usuarios</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Administrar páginas</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Actualizar información de contacto</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Administrar suscriptores</a></li>
+
+			</ul>
+		</nav>
